223adf5c3ef84d12dcdcf65646fd25b220018437
Grinch_ <user.grinch@gmail.com>
2021-08-20 08:36:07 +0600

    update

---
+++ premake5.lua
@@ -0,0 +1,57 @@
+
+----------------------------
+-- Project Generator
+----------------------------
+-- Environment vars
+PSDK_DIR = os.getenv("PLUGIN_SDK_DIR")
+GTASA_DIR = "C:/Work/GTASanAndreas"
+----------------------------
+
+workspace "MenuUI"
+    configurations { "Debug", "Release" }
+    architecture "x86"
+    platforms "Win32"
+    language "C++"
+    cppdialect "C++20"
+    characterset "MBCS"
+    staticruntime "On"
+    location "build"
+
+project "MenuUI"
+    kind "SharedLib"
+    targetdir (GTASA_DIR)
+    targetextension ".asi"
+    
+    files { 
+        "src/**.h", 
+        "src/**.hpp", 
+        "src/**.cpp" 
+    }
+    includedirs {
+        PSDK_DIR .. "/plugin_sa/",
+        PSDK_DIR .. "/plugin_sa/game_sa/",
+        PSDK_DIR .. "/shared/",
+        PSDK_DIR .. "/shared/game/"
+    }
+    libdirs (PSDK_DIR .. "/output/lib")
+    
+    defines { 
+        "NDEBUG",
+        "GTASA",
+        "PLUGIN_SGV_10US",
+        "_CRT_SECURE_NO_WARNINGS",
+        "_CRT_NON_CONFORMING_SWPRINTFS",
+    }
+
+    filter "configurations:Debug"
+        symbols "On"
+        links { 
+            "plugin_d.lib" 
+        }
+
+    filter "configurations:Release"
+        optimize "On"
+        links { 
+            "plugin.lib" 
+        }
+        
